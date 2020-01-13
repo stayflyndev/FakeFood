c8dfbec2b1b8efcd3ca057cfd0d5043770728caa
yourtechsis <aretee64@gmail.com>
2020-01-12 22:29:10 -0700

    Updates

---
+++ precache-manifest.76c9d64260b0977332b774cc6db00355.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb634f7d9ebce4a3f23c74f271f1d3d9",
+    "url": "/FakeFoods/index.html"
+  },
+  {
+    "revision": "5a5d670a81d72e5ebc0e",
+    "url": "/FakeFoods/static/css/main.bfe30c8f.chunk.css"
+  },
+  {
+    "revision": "9ed1f488c03d94d08aef",
+    "url": "/FakeFoods/static/js/2.b9200529.chunk.js"
+  },
+  {
+    "revision": "3d288c697acc5e47e019d9c118e254a6",
+    "url": "/FakeFoods/static/js/2.b9200529.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5a5d670a81d72e5ebc0e",
+    "url": "/FakeFoods/static/js/main.a5623df0.chunk.js"
+  },
+  {
+    "revision": "4b98fa40d191fba9db01",
+    "url": "/FakeFoods/static/js/runtime-main.1c144061.js"
+  },
+  {
+    "revision": "abdc77c731d33342631130a62f19ac72",
+    "url": "/FakeFoods/static/media/icon4.abdc77c7.svg"
+  }
+]);
